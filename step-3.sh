1e1aa231fe034326f6fd1bc96050c7826da89dca
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-01 09:43:26 +0530

    Create step-3.sh

---
+++ step-3.sh
@@ -0,0 +1,12 @@
+    # Initialize kubeadm
+    kubeadm init
+    
+    # If you are the root user, you can run:
+    export KUBECONFIG=/etc/kubernetes/admin.conf     # It will be shown in kubeadm init command output
+    
+    #If you are other than "root" user, run below:    # It will be shown in kubeadm init command output
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
+    
+    #********* Note down the "kubeadm join ...." command, need to fire in worker node for joining in masterNode  # shown in kubeadm init command output
