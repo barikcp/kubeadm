07fab132a5fe9446a7f6e41f9cf035f32f8d6a7c
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-01 09:12:34 +0530

    Install docker

--- step-1.sh
+++ step-1.sh
@@ -15,4 +15,12 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#Install Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#Check Docker process staus
+ ps -eaf|grep docker
+ sudo service docker status  # press q to exit
  
