240d33be5b8cb43ff9987eacba10a93ddd586940
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-01 09:23:53 +0530

    Update step-2.sh

--- step-2.sh
+++ step-2.sh
@@ -10,7 +10,7 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 
-#Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
+#Update apt package index, install kubelet, kubeadm and kubectl
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+
