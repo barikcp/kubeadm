2b8b0f37f20c4c01753c450b36e8fa32732a4afb
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-03 21:07:00 +0530

    kubelet kubeadm kubectl installation

--- step-2.sh
+++ step-2.sh
@@ -6,8 +6,10 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 #Download the Google Cloud public signing key:
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
+
 #Add the Kubernetes apt repository:
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 
 
 #Update apt package index, install kubelet, kubeadm and kubectl
