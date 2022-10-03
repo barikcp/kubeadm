0a495821557b3975ecb08937e414dc7d4a7db27a
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-03 21:01:35 +0530

    Docker installation

--- step-1.sh
+++ step-1.sh
@@ -2,20 +2,20 @@
 sudo apt-get update   # Update the apt package index
   
 # install packages to allow apt to use a repository over HTTPS
-sudo apt-get install \              
-ca-certificates \
-curl \
-gnupg \
-lsb-release ​                                                              
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release                                                              
 
 # Add Docker’s official GPG key:  
-sudo mkdir -p /etc/apt/keyrings​      
+sudo mkdir -p /etc/apt/keyrings      
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg       
  
 # set up the repository
 echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 #Install Docker Engine
 sudo apt-get update
