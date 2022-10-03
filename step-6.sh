0804e51fac406baf1525a2d8a8996553a774a0ee
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-03 23:32:03 +0530

    Install CNI

--- step-6.sh
+++ step-6.sh
@@ -1,2 +1,2 @@
-
- kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+#Install CNI in Master Node.​ (Fire this commands in MasterNode)
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
