45418bcde65e54a338b6fe8f4cf234b6ed317db9
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-03 23:32:39 +0530

    join workernode-2 with MasterNode

--- step-5.sh
+++ step-5.sh
@@ -1,4 +1,4 @@
 # Join the "WorkerNode-2" to MasterNode.​  (Fire this command in WorkerNode-2 only)
 #copy this command from the "kubeadm init" command execution output lastline from MasterNode (like below example)
 #                   kubeadm join 10.160.0.19:6443 --token z5cjqc.n2npw36lyibc8f0h \
-#                               --discovery-token-ca-cert-hash sha256:ecf74dba27b33833762ac39d67974886f61fe93568830763a997c57db9d800c6
+#                              --discovery-token-ca-cert-hash sha256:ecf74dba27b33833762ac39d67974886f61fe93568830763a997c57db9d800c6
